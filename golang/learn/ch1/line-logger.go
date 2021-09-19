fbf7ecda8187c58b2739be60d04f0bb3b7a19c9a
Miracle Nwabueze <mklef121@gmail.com>
2021-09-20 00:21:05 +0100

    Done with chapter 1

--- golang/learn/ch1/line-logger.go
+++ golang/learn/ch1/line-logger.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+)
 
+func main() {
+	LOGFILE := path.Join(os.TempDir(), "mGo.log")
+	f, err := os.OpenFile(LOGFILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer f.Close()
+	LstdFlags := log.Ldate | log.Lshortfile
+	iLog := log.New(f, "LNum ", LstdFlags)
+	iLog.Println("Mastering Go, 3rd edition!")
+	iLog.SetFlags(log.Lshortfile | log.LstdFlags)
+	iLog.Println("Another log entry!")
+
+	log.Println(LOGFILE)
 }
