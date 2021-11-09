60411ace194b09815ea6fe62eb1487e94c0e2e09
Miracle Nwabueze <mklef121@gmail.com>
2021-11-09 08:45:47 +0100

    start linked list implementation

--- golang/DSA/data-structures/linked-lists/linked-list.go
+++ golang/DSA/data-structures/linked-lists/linked-list.go
@@ -1,16 +1,63 @@
 package main
 
+import "fmt"
+
 type Node struct {
-	value interface{}
 	next  *Node
+	value int
+}
+
+func (node *Node) setNext(next *Node) {
+	node.next = next
 }
 
 type LinkedList struct {
 	head   *Node
-	length int
 	tail   *Node
+	length int
+}
+
+func (list *LinkedList) prepend(node *Node) *LinkedList {
+	node.next = list.head
+	list.head = node
+	list.length++
+
+	return list
+}
+
+func (list LinkedList) printList() {
+	current := list.head
+	for list.length != 0 {
+		fmt.Printf("%d ", current.value)
+		current = current.next
+		list.length--
+	}
+
+	fmt.Println("")
+}
+func CreateList(value int) *LinkedList {
+	head := Node{
+		value: value,
+		next:  nil,
+	}
+
+	list := LinkedList{
+		head:   &head,
+		tail:   &head,
+		length: 1,
+	}
+
+	return &list
+
 }
 
 func main() {
+	mylist := CreateList(34)
+
+	mylist.prepend(&Node{value: 45}).prepend(&Node{value: 50}).prepend(&Node{value: 60})
+
+	mylist.printList()
+
+	fmt.Println(mylist)
 
 }
