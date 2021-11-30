1b063329dab487fe052b2440634ef4d1b4a03f5c
Miracle Nwabueze <mklef121@gmail.com>
2021-11-30 09:27:52 +0100

    feat: round up json and json streams

--- golang/learn/ch6/json-streams.go
+++ golang/learn/ch6/json-streams.go
@@ -60,9 +60,55 @@ func main() {
 	}
 
 	fmt.Print("After Serialize:", buf)
+
+	decoder := json.NewDecoder(buf)
+	var temp []Data
+	err = DeSerialize(decoder, &temp)
+	fmt.Println("After DeSerialize:")
+	for index, value := range temp {
+		fmt.Println(index, value)
+	}
+
+	fmt.Println("Last record:", t)
+	_ = PrettyPrint(t)
+
+	val, _ := JSONstream(DataRecords)
+	fmt.Println(val)
 }
 
 // Serialize serializes a slice with JSON records
+// The Serialize() function accepts two parameters, a *json.Encoder and a slice of any data type,
+// hence the use of interface{}. The function processes the slice and writes the output to the buffer of the
+// json.Encoder—this buffer is passed as a parameter to the encoder at the time of its creation.
 func Serialize(e *json.Encoder, slice interface{}) error {
 	return e.Encode(slice)
+}
+
+// DeSerialize decodes a serialized slice with JSON records
+func DeSerialize(e *json.Decoder, slice interface{}) error {
+	return e.Decode(slice)
+}
+
+//  Pretty printing JSON records\\
+
+//This subsection illustrates how to pretty print JSON records, which means printing JSON records
+//in a pleasant and readable format without knowing the format of the Go structure that holds the JSON records.
+func PrettyPrint(v interface{}) (err error) {
+	b, err := json.MarshalIndent(v, "", "\t")
+	if err == nil {
+		fmt.Println(string(b))
+	}
+	return err
+}
+
+func JSONstream(data interface{}) (string, error) {
+	buffer := new(bytes.Buffer)
+	encoder := json.NewEncoder(buffer)
+	encoder.SetIndent("", "\t")
+
+	err := encoder.Encode(data)
+	if err != nil {
+		return "", err
+	}
+	return buffer.String(), nil
 }
